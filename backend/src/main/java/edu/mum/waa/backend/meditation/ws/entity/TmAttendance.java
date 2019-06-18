24263c9bc989134c49aad0dd44ec006ab7ad5afd
bochirkhuyag <49167544+bochirkhuyag@users.noreply.github.com>
2019-06-18 16:13:44 -0500

    attendance score added to data.sql

--- backend/src/main/java/edu/mum/waa/backend/meditation/ws/entity/TmAttendance.java
+++ backend/src/main/java/edu/mum/waa/backend/meditation/ws/entity/TmAttendance.java
@@ -18,7 +18,7 @@ import java.time.LocalDate;
 public class TmAttendance{
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "Student_Id")
