447d098f85b9ca8abdd6867fc2c45b7f0472f2fc
DmaC <demon.005.gl@gmail.com>
2019-06-19 16:34:41 -0500

    file upload

--- frontend/src/main/resources/static/js/main.js
+++ frontend/src/main/resources/static/js/main.js
@@ -5,4 +5,48 @@ $(document).ready(function() {
         buttons: [ 'copy', 'csv', 'excel' ]
     });
 
+    $("#btnSubmit").click(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        fire_ajax_submit();
+
+    });
+
 });
+
+function fire_ajax_submit() {
+
+    let fd = new FormData();
+    let file = document.forms['fileForm']['file'].files[0];
+    fd.append( 'file', file);
+
+    $("#btnSubmit").prop("disabled", true);
+
+    $.ajax({
+        type: "POST",
+        enctype: 'multipart/form-data',
+        url: "http://localhost:8082/uploadFile",
+        data: fd,
+        processData: false,
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+
+            $("#result").text(data);
+            console.log("SUCCESS : ", data);
+            $("#btnSubmit").prop("disabled", false);
+
+        },
+        error: function (e) {
+
+            $("#result").text(e.responseText);
+            console.log("ERROR : ", e);
+            $("#btnSubmit").prop("disabled", false);
+
+        }
+    });
+
+}
