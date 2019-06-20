401dbf7bfadd20eb02368aede7245171993a48b2
anherdene <anherdene@gmail.com>
2019-06-19 21:31:53 -0500

    block crud and block changed to long

--- backend/src/main/java/edu/mum/waa/backend/meditation/ws/controller/BlockController.java
+++ backend/src/main/java/edu/mum/waa/backend/meditation/ws/controller/BlockController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/crud/block")
+@RequestMapping("/crud/blocks")
 @PreAuthorize("hasRole('USER')")
 public class BlockController extends RestCrudController<Block> {
     private static final Logger logger = LoggerFactory.getLogger(BlockController.class);
